ece91966602596a505fabce867ca2c646808d177
michnguyen <michnguyen@deloitte.dk>
2022-11-28 14:23:21 +0100

    Final script with seperat function python

--- Functions.py
+++ Functions.py
@@ -1,18 +1,96 @@
+import requests
 
-def limit_stock(price, stock, threshold):
-    if price <= threshold:
-        buy_stock(price, stock)
+# Use FX rate
+fx_rates = {
+    "USD/EUR": 0.96,
+    "DKK/EUR": 0.13,
+    "GBP/EUR": 1.16
+}
+#
+# input:
+# users_info: dictionary consisting of mulitple users information
+# current_user: string with a user name from the users info
+
+
+def buy_stock(users_info, current_user):
+    keyword = input("Give keyword (symbol/name)")
+    url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + keyword + "&apikey=KOA81QCGI4HZS0CJ"
+    r = requests.get(url)
+    data = r.json()
+    for i in data["bestMatches"]:
+        symbol = (i["1. symbol"])
+        name = (i["2. name"])
+        print(symbol, name)
+
+    stock = input("which ticker?")
+    response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stock + "&interval=5min&outputsize=full&apikey=KOA81QCGI4HZS0CJ")
+    raw_data = response.json()
+    stock_price = int(float(raw_data['Time Series (5min)']['2022-11-23 17:40:00']['4. close']))
+
+    quant = int(input('Enter buying quantity: '))
+    response_cur = requests.get(
+        "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + stock + "&apikey=KOA81QCGI4HZS0CJ")
+    raw_data_cur = response_cur.json()
+    stock_cur = raw_data_cur["bestMatches"][0]["8. currency"]
+
+
+    # Do not buy stock if the stock*quant is higher than our portfolio
+    if (stock_price*quant) > users_info[current_user]['balance (EUR)']:
+        print("You don't have enough money")
     else:
-        print("It's too expensive girl, don't buy...sell!")
-        sell_stock(price,stock)
-
-def stop_order(price, stock, stop_threshold):
-     if price == stop_threshold:
-        print("You can buy or sell the stocks.")
-        user_input = input("Please give a buy or sell argument: ")
-        if user_input == "buy":
-            buy_stock(price, stock)
+        # remember that these stock are not owned by the user
+        if stock in users_info[current_user]['portfolio']:
+            users_info[current_user]['portfolio'][stock] += quant
         else:
-            sell_stock(price, stock)
+            users_info[current_user]['portfolio'][stock] = quant
+
+        if stock_cur == "USD":
+            eur_stock_price = fx_rates["USD/EUR"] * stock_price
+            users_info[current_user]['balance (EUR)'] -= eur_stock_price * quant
+        elif stock_cur == "DKK":
+            dkk_stock_price = fx_rates["DKK/EUR"] * stock_price
+            users_info[current_user]['balance (EUR)'] -= dkk_stock_price * quant
+        elif stock_cur == "GBP":
+            gbp_stock_price = fx_rates["DKK/EUR"] * stock_price
+            users_info[current_user]['balance (EUR)'] -= gbp_stock_price * quant
+        else:
+            print("currency not found")
+
+def sell_stock(users_info, current_user):
+    keyword = input("Give keyword (symbol/name)")
+    url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + keyword + "&apikey=KOA81QCGI4HZS0CJ"
+    r = requests.get(url)
+    data = r.json()
+    for i in data["bestMatches"]:
+        symbol = (i["1. symbol"])
+        name = (i["2. name"])
+        print(symbol, name)
+
+    stock = input("which ticker? ")
+    response = requests.get(
+        "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stock + "&interval=5min&outputsize=full&apikey=KOA81QCGI4HZS0CJ")
+    raw_data = response.json()
+    stock_price = int(float(raw_data['Time Series (5min)']['2022-11-23 17:40:00']['4. close']))
+    quant = int(input('Enter selling quantity: '))
+    # remember that these stock are not owned by the user
+
+    response_cur = requests.get(
+        "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + stock + "&apikey=KOA81QCGI4HZS0CJ")
+    raw_data_cur = response_cur.json()
+    stock_cur = raw_data_cur["bestMatches"][0]["8. currency"]
+
+    if (stock in users_info[current_user]['portfolio']) and (users_info[current_user]['portfolio'][stock] >= quant):
+        users_info[current_user]['portfolio'][stock] -= quant
+        if stock_cur == "USD":
+            eur_stock_price = fx_rates["USD/EUR"] * stock_price
+            users_info[current_user]['balance (EUR)'] += eur_stock_price * quant
+        elif stock_cur == "DKK":
+            dkk_stock_price = fx_rates["DKK/EUR"] * stock_price
+            users_info[current_user]['balance (EUR)'] += dkk_stock_price * quant
+        elif stock_cur == "GBP":
+            gbp_stock_price = fx_rates["DKK/EUR"] * stock_price
+            users_info[current_user]['balance (EUR)'] += gbp_stock_price * quant
+        else:
+            print("currency not found")
     else:
-        print("The stock threshold has not been reached yet.")
+        print("You don't own enough stocks!")
--- Main.py
+++ Main.py
@@ -1,87 +1,20 @@
-import requests
-import pandas as pd
+from Functions import *
 
-import requests
-
+# Get user info
 users_info = {
     "Alexia": {"balance (EUR)":10000, "portfolio":{}},
     "Christopher": {"balance (EUR)":10000, "portfolio":{}},
     "Michelle": {"balance (EUR)":10000, "portfolio":{}}
 }
 
-fx_rates = {
-    "USD/EUR": 0.96,
-    "DKK/EUR": 0.13,
-    "GBP/EUR": 1.16
-}
-
-def buy_stock():
-    stock = input("which stock? ")
-    response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stock + "&interval=5min&outputsize=full&apikey=KOA81QCGI4HZS0CJ")
-    raw_data = response.json()
-    stock_price = int(float(raw_data['Time Series (5min)']['2022-11-23 17:40:00']['4. close']))
-    quant = int(input('Enter buying quantity: '))
-    response_cur = requests.get(
-        "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + stock + "&apikey=KOA81QCGI4HZS0CJ")
-    raw_data_cur = response_cur.json()
-    stock_cur = raw_data_cur["bestMatches"][0]["8. currency"]
-
-    # remember that these stock are not owned by the user
-    print(stock_price*quant)
-    if stock in users_info[current_user]['portfolio']:
-        users_info[current_user]['portfolio'][stock] += quant
-    else:
-        users_info[current_user]['portfolio'][stock] = quant
-
-    if stock_cur == "USD":
-        eur_stock_price = fx_rates["USD/EUR"] * stock_price
-        users_info[current_user]['balance (EUR)'] -= eur_stock_price * quant
-    elif stock_cur == "DKK":
-        dkk_stock_price = fx_rates["DKK/EUR"] * stock_price
-        users_info[current_user]['balance (EUR)'] -= dkk_stock_price * quant
-    elif stock_cur == "GBP":
-        gbp_stock_price = fx_rates["DKK/EUR"] * stock_price
-        users_info[current_user]['balance (EUR)'] -= gbp_stock_price * quant
-    else:
-        print("currency not found")
-
-def sell_stock():
-    stock = input("which stock? ")
-    response = requests.get(
-        "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stock + "&interval=5min&outputsize=full&apikey=KOA81QCGI4HZS0CJ")
-    raw_data = response.json()
-    stock_price = int(float(raw_data['Time Series (5min)']['2022-11-23 17:40:00']['4. close']))
-    print(stock_price)
-    quant = int(input('Enter selling quantity: '))
-    # remember that these stock are not owned by the user
-    users_info[current_user]['portfolio'][stock] -= quant
-    response_cur = requests.get(
-        "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + stock + "&apikey=KOA81QCGI4HZS0CJ")
-    raw_data_cur = response_cur.json()
-    stock_cur = raw_data_cur["bestMatches"][0]["8. currency"]
-    print(stock_cur)
-
-    if stock_cur == "USD":
-        eur_stock_price = fx_rates["USD/EUR"]*stock_price
-        users_info[current_user]['balance (EUR)'] += eur_stock_price * quant
-    elif stock_cur == "DKK":
-        dkk_stock_price = fx_rates["DKK/EUR"] * stock_price
-        users_info[current_user]['balance (EUR)'] += dkk_stock_price * quant
-    elif stock_cur == "GBP":
-        gbp_stock_price = fx_rates["DKK/EUR"] * stock_price
-        users_info[current_user]['balance (EUR)'] += gbp_stock_price * quant
-    else:
-        print("currency not found")
-
-
 name = input("What is your name?")
 current_user = name
 while True:
     choice = input("What do you want to do? (buy/sell/new_user/quit) ")
     if choice == "buy":
-        buy_stock()
+        buy_stock(users_info, current_user)
     elif choice == "sell":
-        sell_stock()
+        sell_stock(users_info, current_user)
     elif choice == "new_user":
         name = input("What is your name?")
         current_user = name
@@ -89,27 +22,7 @@ while True:
         break
     print(users_info)
 
-import requests
-keyword = input("Give keyword (symbol/name)")
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + keyword + "&apikey=KOA81QCGI4HZS0CJ"
-r = requests.get(url)
-data = r.json()
 
-#print(data)
 
-print(data["bestMatches"])
-for i in data["bestMatches"]:
-    print(data["bestMatches"][])
-#print(data)
-#'FRD'
-def fund():
-    my_list = list(cr)
-    symbol = input("Give the symbol: ")
- #   for symbol in my_list[0]:
-    print(f"The information is: {my_list[0:6]}")
-
-fund()
-print(my_list[0][0])
 
 
